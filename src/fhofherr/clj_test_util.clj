58871b447f327b881c944844be53faa8c27d6328
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-04-06 09:34:56 +0200

    Add simple before-each-fixture.

--- src/fhofherr/clj_test_util.clj
+++ src/fhofherr/clj_test_util.clj
@@ -1,9 +1,28 @@
 (ns fhofherr.clj-test-util)
 
-(defn wrap-test
-  [f t]
-  {:pre [(:test (meta t))]}
-  (let [act-test (:test (meta t))
-        wrapper (fn [] (f act-test))
-        wrap (fn [cur-meta] (assoc cur-meta :test wrapper))]
-    (vary-meta t wrap)))
+(defmacro fixture
+  "TODO: multiple before each fixtures ==> in definition order
+  TODO: multiple after each fixtures ==> in definition order
+  TODO: multiple arount each fixtures ==> in definition order
+  TODO: mixture of before, after, around each ==> before-each first,
+  then around-each, then after-each
+  TODO: after each even if around hook, or test itself fail
+
+  TODO: be carefull if befor each fails, nothing else will be executed (danger
+  if using multiple before hooks)
+  "
+  [fixture-name fixture-fns & definitions]
+  ;; TODO IllegalArgumnentException
+  (assert (even? (count fixture-fns))
+          "fixture requires an even number of definitions in fixture-fns!")
+  (let [before-fixture (gensym "before-fixture")]
+    `(let [~before-fixture ~(second fixture-fns)]
+       ~@(for [definition definitions]
+           `(let [test-var# ~definition
+                  test# (:test (meta test-var#))]
+              (alter-meta! test-var#
+                           assoc
+                           :test (fn [] (~before-fixture) (test#)))
+              test-var#)
+           )
+       )))
--- test/fhofherr/t_clj_test_util.clj
+++ test/fhofherr/t_clj_test_util.clj
@@ -2,19 +2,14 @@
   (:require [clojure.test :refer :all]
             [fhofherr.clj-test-util :as test-util]))
 
-;; Since we write helper functions that manipulate clojure.test tests we
-;; need some test to manipulate. It does not have any meaning of itself.
-(defn create-some-test
-  [tracker sig-kw]
-  (with-meta (fn [] :something)
-             {:test (fn []
-                      (swap! tracker conj sig-kw)
-                      (is (= 1 1)))}))
+(defn- register-call
+  [calls kw]
+  (fn [] (swap! calls conj kw)))
 
-(deftest wrap-test-in-function
-  (let [tracker (atom [])
-        f (fn [t] (t))
-        wrapped-test (test-util/wrap-test f (create-some-test tracker
-                                                              :test-called))]
-    (test wrapped-test)
-    (is (= @tracker [:test-called]))))
+(def fixture-calls (atom []))
+(test-util/fixture
+  before-each
+  [:before-each (register-call fixture-calls :before-each-called)]
+
+  (deftest before-each-fixture
+    (is (= [:before-each-called] @fixture-calls))))
