6aa0c0a1d14ebd344c7448fe0a5f0f308e8a9f40
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-04-06 10:38:23 +0200

    Support after-each fixtures.

--- src/fhofherr/clj_test_util.clj
+++ src/fhofherr/clj_test_util.clj
@@ -11,7 +11,8 @@
 
 (defn compose-fixtures
   [fixtures]
-  (let [composers {:before-each chain-fns}
+  (let [composers {:before-each chain-fns
+                   :after-each chain-fns}
         do-compose (fn [acc [fixture-type f]]
                      (as-> acc $
                        (fixture-type $)
@@ -19,16 +20,20 @@
                        (assoc acc fixture-type $)))]
     (->> fixtures
        (partition 2)
-       (reduce do-compose {:before-each noop}))))
+       (reduce do-compose {:before-each noop
+                           :after-each noop}))))
 
 (defn- emit-definition-wrapper
-  [definitions before-each]
+  [definitions before-each after-each]
   (for [definition definitions]
     `(let [test-var# ~definition
            test# (:test (meta test-var#))]
        (alter-meta! test-var#
                     assoc
-                    :test (fn [] (~before-each) (test#)))
+                    :test (fn []
+                            (~before-each)
+                            (test#)
+                            (~after-each)))
        test-var#)
     )
   )
@@ -49,8 +54,10 @@
   (assert (even? (count fixture-fns))
           "fixture requires an even number of definitions in fixture-fns!")
   (let [fixtures (gensym "fixtures")
-        before-each (gensym "before-each")]
+        before-each (gensym "before-each")
+        after-each (gensym "after-each")]
     `(let [~fixtures (compose-fixtures ~fixture-fns)
-           ~before-each (:before-each ~fixtures)]
-       ~@(emit-definition-wrapper definitions before-each)
+           ~before-each (:before-each ~fixtures)
+           ~after-each (:after-each ~fixtures)]
+       ~@(emit-definition-wrapper definitions before-each after-each)
        )))
--- test/fhofherr/t_clj_test_util.clj
+++ test/fhofherr/t_clj_test_util.clj
@@ -15,14 +15,39 @@
           composed (test-util/compose-fixtures fixtures)]
       ((:before-each composed))
       (is (= [:before-each-1 :before-each-2] @calls))))
-  )
 
-(def fixture-calls (atom []))
+  (testing "after-each fixture composition"
+    (let [calls (atom [])
+          fixtures [:after-each (register-call calls :after-each-1)
+                    :after-each (register-call calls :after-each-2)]
+          composed (test-util/compose-fixtures fixtures)]
+      ((:after-each composed))
+      (is (= [:after-each-1 :after-each-2]
+             @calls)))))
 
+(def before-each-calls (atom []))
 (test-util/fixture
   before-each
-  [:before-each (register-call fixture-calls :before-each-1)
-   :before-each (register-call fixture-calls :before-each-2)]
+  [:before-each (register-call before-each-calls :before-each-1)
+   :before-each (register-call before-each-calls :before-each-2)]
 
   (deftest before-each-fixture
-    (is (= [:before-each-1 :before-each-2] @fixture-calls))))
+    (is (= [:before-each-1 :before-each-2] @before-each-calls))))
+
+;; Since the after-each fixtures won't be called until the test passed
+;; we can't test this from within the test itself. We therefore write a
+;; dummy test, which we call again in the follwoing test.
+(def after-each-calls (atom []))
+(test-util/fixture
+  after-each-prep
+  [:after-each (register-call after-each-calls :after-each-1)
+   :after-each (register-call after-each-calls :after-each-2)]
+  (deftest after-each-fixture-prep
+    (is (= 1 1))))
+
+(deftest after-each
+  ;; Call the after-each-fixture-prep test from above.
+  (reset! after-each-calls [])
+  (after-each-fixture-prep)
+
+  (is (= [:after-each-1 :after-each-2] @after-each-calls)))
