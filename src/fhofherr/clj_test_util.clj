114113a9a149e19ed02ade1aa4897d53169ae5a2
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-04-06 11:51:41 +0200

    Support around-each fixtures.

--- src/fhofherr/clj_test_util.clj
+++ src/fhofherr/clj_test_util.clj
@@ -9,10 +9,26 @@
     (f1)
     (f2)))
 
+(defn fncall
+  [f]
+  (f))
+
+(defn- comp-around-fns
+  "Compose two around-each fixture functions.
+
+  Returns a new around-each fixture function expecting a `test-fn`. The returned
+  function creates a function which applies the `inner-fn` to the `test-fn`.
+  This function is then passed to the `outer-fn`."
+  [outer-fn inner-fn]
+  (fn [test-fn]
+    (let [new-test-fn #(inner-fn test-fn)]
+      (outer-fn new-test-fn))))
+
 (defn compose-fixtures
   [fixtures]
   (let [composers {:before-each chain-fns
-                   :after-each chain-fns}
+                   :after-each chain-fns
+                   :around-each comp-around-fns}
         do-compose (fn [acc [fixture-type f]]
                      (as-> acc $
                        (fixture-type $)
@@ -21,19 +37,20 @@
     (->> fixtures
        (partition 2)
        (reduce do-compose {:before-each noop
-                           :after-each noop}))))
+                           :after-each noop
+                           :around-each fncall}))))
 
 (defn wrap-test
-  [before actual-test after]
+  [before-each around-each actual-test after-each]
   (fn []
-    (before)
+    (before-each)
     (try
-      (actual-test)
+      (around-each actual-test)
       (finally
-        (after)))))
+        (after-each)))))
 
 (defn- emit-definition-wrapper
-  [definitions before-each after-each]
+  [definitions before-each around-each after-each]
   (for [definition definitions]
     `(let [test-var# ~definition
            test# (:test (meta test-var#))]
@@ -41,6 +58,7 @@
                     assoc
                     :test (wrap-test
                             ~before-each
+                            ~around-each
                             test#
                             ~after-each))
        test-var#)))
@@ -62,8 +80,10 @@
           "fixture requires an even number of definitions in fixture-fns!")
   (let [fixtures (gensym "fixtures")
         before-each (gensym "before-each")
-        after-each (gensym "after-each")]
+        after-each (gensym "after-each")
+        around-each (gensym "around-each")]
     `(let [~fixtures (compose-fixtures ~fixture-fns)
            ~before-each (:before-each ~fixtures)
-           ~after-each (:after-each ~fixtures)]
-       ~@(emit-definition-wrapper definitions before-each after-each))))
+           ~after-each (:after-each ~fixtures)
+           ~around-each (:around-each ~fixtures)]
+       ~@(emit-definition-wrapper definitions before-each around-each after-each))))
--- test/fhofherr/t_clj_test_util.clj
+++ test/fhofherr/t_clj_test_util.clj
@@ -3,8 +3,14 @@
             [fhofherr.clj-test-util :as test-util]))
 
 (defn- register-call
-  [calls kw]
-  (fn [] (swap! calls conj kw)))
+  ([calls kw]
+   (fn [] (swap! calls conj kw)))
+
+  ([calls before-kw after-kw]
+   (fn [f]
+     (swap! calls conj before-kw)
+     (f)
+     (swap! calls conj after-kw))))
 
 (deftest fixture-composition
 
@@ -23,6 +29,23 @@
           composed (test-util/compose-fixtures fixtures)]
       ((:after-each composed))
       (is (= [:after-each-1 :after-each-2]
+             @calls))))
+
+  (testing "around-each fixture composition"
+    (let [calls (atom [])
+          fixtures [:around-each (register-call calls
+                                                :around-each-1-before
+                                                :around-each-1-after)
+                    :around-each (register-call calls
+                                                :around-each-2-before
+                                                :around-each-2-after)]
+          composed (test-util/compose-fixtures fixtures)]
+      ((:around-each composed) (fn [] nil))
+
+      (is (= [:around-each-1-before
+              :around-each-2-before
+              :around-each-2-after
+              :around-each-1-after]
              @calls)))))
 
 (deftest wrap-test
@@ -32,7 +55,10 @@
           before-each (register-call calls :before-each)
           the-test (register-call calls :the-test)
           after-each (register-call calls :after-each)
-          wrapped-test (test-util/wrap-test before-each the-test after-each)]
+          wrapped-test (test-util/wrap-test before-each
+                                            test-util/fncall
+                                            the-test
+                                            after-each)]
       (wrapped-test)
       (is (= [:before-each :the-test :after-each] @calls))))
 
@@ -41,7 +67,10 @@
           before-each (register-call calls :before-each)
           failing-test (fn [] (throw (Throwable. "Ooops!")))
           after-each (register-call calls :after-each)
-          wrapped-test (test-util/wrap-test before-each failing-test after-each)]
+          wrapped-test (test-util/wrap-test before-each
+                                            test-util/fncall
+                                            failing-test
+                                            after-each)]
       (try
         (wrapped-test)
         (catch Throwable _))
@@ -75,3 +104,24 @@
   (after-each-fixture-prep)
 
   (is (= [:after-each-1 :after-each-2] @after-each-calls)))
+
+(def around-each-calls (atom []))
+(test-util/fixture
+  around-each-dummy
+  [:around-each (register-call around-each-calls
+                               :before-around-each-1
+                               :after-around-each-1)
+   :around-each (register-call around-each-calls
+                               :before-around-each-2
+                               :after-around-each-2)]
+  (deftest around-each-fixture-prep
+    (is (= 1 1))))
+
+(deftest around-each
+  (reset! around-each-calls [])
+  (around-each-fixture-prep)
+  (is (= [:before-around-each-1
+          :before-around-each-2
+          :after-around-each-2
+          :after-around-each-1]
+         @around-each-calls)))
