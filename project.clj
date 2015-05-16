3e37a5ad5d157439796d17d77c4ac74e52824cca
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2015-05-16 14:43:54 +0200

    Add scm info.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject fhofherr/clj-test-util "0.1.0-SNAPSHOT"
   :description "Helpers and utilities for clojure test"
   :url "https://github.com/fhofherr/clj-test-util"
+  :scm {:name "git"
+        :url "https://github.com/fhofherr/clj-test-util"}
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
